a5ad05edb317b5d31e0dca00280b7dc9196829c7
Cool-Dude-x <69380703+Cool-Dude-x@users.noreply.github.com>
2020-08-22 19:54:51 +0530

    Update command_filter.py

--- commands/command_filter.py
+++ commands/command_filter.py
@@ -1,20 +1,9 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ParseMode
 import os
 
-source="https://github.com/Artis7eeR/forward-Tag-Remover-Bot"
 START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do"
 HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 
-#Inline Keyboard Button
-keyboard = [
-[
- InlineKeyboardButton("Source Code", url=source)
-],
-[
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
-]
-]
-reply_markup = InlineKeyboardMarkup(keyboard)
 
 #Start Message
 def start_text(u,c):
